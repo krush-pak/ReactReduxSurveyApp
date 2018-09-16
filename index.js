7ef1ac077674a032f73a578308f7cc0c7e9911a1
Jonas Jerusalem <Jonny@Macs-MacBook-Pro.local>
2018-09-16 10:21:12 +0200

    changed content

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'Buddy' });
+app.get("/", (req, res) => {
+  res.send({ hi: "Man" });
 });
 
 const PORT = process.env.PORT || 5000;
