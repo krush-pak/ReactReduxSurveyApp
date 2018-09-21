829aac0c9dc9dda2183c13bb928d7f51f4ad75a5
Jonas Jerusalem <Jonny@Macs-MacBook-Pro.local>
2018-09-21 05:01:04 +0200

    finalized google oauth logic and refactored folder structure

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 const express = require("express");
-const passpoart = require("passport");
-const GoogleStrategy = require("passport-google.oauth20").Strategy;
-const keys = require("./config/keys");
+require("./services/passport.js");
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    accessToken => {
-      console.log(accessToken);
-    }
-  )
-);
+//Routes
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
