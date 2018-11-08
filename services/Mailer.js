3014bc7b0cb54c1906ca132cc77011af37a6cca8
Jonas Jerusalem <Jonny@Macs-MacBook-Pro.local>
2018-11-08 08:07:20 +0100

    added mail creation logic to survey route, finished email template setup and mailer config, tested sending mails

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email("no-reply@emaily.com");
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
@@ -34,9 +35,20 @@ class Mailer extends helper.Mail {
   addRecipients() {
     const personalize = new helper.Personalization();
     this.recipients.forEach(recipients => {
-      personalize.addTo(recipient);
+      personalize.addTo(recipients);
     });
     this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: "POST",
+      path: "/v3/mail/send",
+      body: this.toJSON()
+    });
+
+    const response = await this.sgApi.API(request);
+    return response;
   }
 }
 
