3231b4e930e790bda672fa099169c550002faed5
Jonas Jerusalem <Jonny@Macs-MacBook-Pro.local>
2018-11-14 07:44:16 +0100

    installed and setup redux-form and added basic form components

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,11 @@
+//Survey New shows Survey Form and Survey Form Review
+import React, { Component } from "react";
+import SurveyForm from "./SurveyForm";
+
+class SurveyNew extends Component {
+  render() {
+    return <SurveyForm />;
+  }
+}
+
+export default SurveyNew;
