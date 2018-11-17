a02f5b17c82a5160402695c6939ebb5b0e1101dc
Jonas Jerusalem <Jonny@Macs-MacBook-Pro.local>
2018-11-17 08:04:12 +0100

    setup review functionality for surveyForm Inputs

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,10 +1,30 @@
 //Survey New shows Survey Form and Survey Form Review
 import React, { Component } from "react";
 import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
 
 class SurveyNew extends Component {
+  //componentLevel State with createReactApp
+  state = { showFormReview: false };
+
+  renderContent() {
+    if (this.state.showFormReview) {
+      return (
+        <SurveyFormReview
+          onCancel={() => this.setState({ showFormReview: false })}
+        />
+      );
+    }
+
+    return (
+      <SurveyForm
+        onSurveySubmit={() => this.setState({ showFormReview: true })}
+      />
+    );
+  }
+
   render() {
-    return <SurveyForm />;
+    return <div>{this.renderContent()}</div>;
   }
 }
 
