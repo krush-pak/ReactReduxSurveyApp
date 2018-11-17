be703b9ef7d61473aac006ebff2641ece21f0373
Jonas Jerusalem <Jonny@Macs-MacBook-Pro.local>
2018-11-17 19:18:15 +0100

    added logic to dump form values when surveyNew component unmounts

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,5 +1,6 @@
 //Survey New shows Survey Form and Survey Form Review
 import React, { Component } from "react";
+import { reduxForm } from "redux-form";
 import SurveyForm from "./SurveyForm";
 import SurveyFormReview from "./SurveyFormReview";
 
@@ -28,4 +29,6 @@ class SurveyNew extends Component {
   }
 }
 
-export default SurveyNew;
+export default reduxForm({
+  form: "surveyForm"
+})(SurveyNew);
